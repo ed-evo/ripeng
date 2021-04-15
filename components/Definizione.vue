26ef11a98abe5486b5803a4db0dda272656c8e27
Lingyong Sun <lysz210@gmail.com>
2021-04-15 21:15:27 +0200

    creazione Definizioni
    creazione aggiunta definizioni semplici

---
+++ components/Definizione.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-text-field
+    :rules="[check]"
+    :label="italiano"
+  ></v-text-field>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  props: ['def'],
+  computed: {
+    inglese (): string {
+      return this.def ? this.def.eng : ''
+    },
+    italiano (): string {
+      return this.def ? this.def.it : ''
+    }
+  },
+  methods: {
+    check (value: string): boolean {
+      return value ? value === this.inglese : true
+    }
+  }
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,89 +1,88 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
+  <v-row>
+    <v-col class="text-center">
       <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
         <v-card-actions>
-          <v-spacer />
+          <definizione
+            :def="definizione"
+          ></definizione>
           <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
+            @click="refreshDef"
           >
-            Continue
+            avanti
           </v-btn>
         </v-card-actions>
       </v-card>
+      <definizione
+        v-for="def in defs"
+        :key="def.eng"
+        :def="def"
+      ></definizione>
     </v-col>
   </v-row>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+<script lang="ts">
+import { shuffle, sample } from 'lodash'
+import { defineComponent } from '@vue/composition-api'
+import Definizione from '~/components/Definizione.vue'
 
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
+class Def {
+  constructor (public eng: string, public it: string) {}
 }
+
+const DEFS = [
+  new Def('beard', 'barba'),
+  new Def('curly', 'ricciolini'),
+  new Def('freckles', 'lentiggini'),
+  new Def('glasses', 'occhiali'),
+  new Def('good-looking', 'bell\'aspetto'),
+  new Def('moustache', 'baffi'),
+  new Def('plump', 'paffuto'),
+  new Def('short', 'basso'),
+  new Def('slim', 'magro'),
+  new Def('spiky', 'sparati'),
+  new Def('stocky', 'tarchiato'),
+  new Def('straight', 'dritto'),
+  new Def('tall', 'alto'),
+  new Def('wavy', 'ondulato'),
+  new Def('ankle', 'caviglia'),
+  new Def('arm', 'braccio'),
+  new Def('chest', 'torace'),
+  new Def('chin', 'mento'),
+  new Def('ear', 'orecchio'),
+  new Def('elbow', 'gomito'),
+  new Def('eye', 'occhio'),
+  new Def('eyebrow', 'sopracciglio'),
+  new Def('foot', 'piede'),
+  new Def('feet', 'piedi'),
+  new Def('forehead', 'fronte'),
+  new Def('hand', 'mano'),
+  new Def('head', 'testa'),
+  new Def('knee', 'ginocchio'),
+  new Def('leg', 'gamba'),
+  new Def('mouth', 'bocca'),
+  new Def('neck', 'collo'),
+  new Def('nose', 'naso'),
+  new Def('shoulder', 'spalla')
+]
+
+export default defineComponent({
+  components: {
+    Definizione
+  },
+  data () {
+    return {
+      defs: shuffle(DEFS),
+      definizione: sample(DEFS),
+      value: ''
+    }
+  },
+  methods: {
+    refreshDef () {
+      this.value = ''
+      this.definizione = sample(DEFS)
+    }
+  }
+})
 </script>
