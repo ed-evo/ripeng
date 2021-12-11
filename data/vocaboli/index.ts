bb9fec9ad2f45337dcafa6bf7b18343a661ce950
Lingyong Sun <lysz210@gmail.com>
2021-12-11 19:23:03 +0100

    add houswork & trasportations

--- data/vocaboli/index.ts
+++ data/vocaboli/index.ts
@@ -2,9 +2,13 @@ import Definizione from '~/model/Definizione';
 import corpo from './corpo.json';
 import emozioni from './emozioni.json';
 import holidays from './holidays.json';
+import trasporti from './trasporti.json';
+import housework from './housework.json';
 
 export default {
   corpo,
   emozioni,
   holidays,
+  trasporti,
+  housework
 } as { [key: string]: Definizione[] }
