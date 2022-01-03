657bcece32d9dc9a9b1e8d59eafa9d56cfc50133
Lingyong Sun <lysz210@gmail.com>
2022-01-03 20:39:24 +0100

    add weather

--- data/vocaboli/index.ts
+++ data/vocaboli/index.ts
@@ -4,11 +4,13 @@ import emozioni from './emozioni.json';
 import holidays from './holidays.json';
 import trasporti from './trasporti.json';
 import housework from './housework.json';
+import weather from './weather.json';
 
 export default {
   corpo,
   emozioni,
   holidays,
   trasporti,
-  housework
+  housework,
+  weather
 } as { [key: string]: Definizione[] }
